4bb90217cfd6fa4b56d54462b46c2895273323e9
Matt Stow <matt@mattstow.com>
2017-12-20 12:37:32 +1100

    Don't choke on invalid data structures

--- src/convertDataToObject.js
+++ src/convertDataToObject.js
@@ -5,24 +5,34 @@ const convertDataToObject = (app) => {
   let rData;
 
   if (app.attributes['q-data']) {
-    data = JSON.parse(app.attributes['q-data'].value);
+    try {
+      data = JSON.parse(app.attributes['q-data'].value);
+    }
+    catch (e) {
+      // Do nothing
+    }
   }
 
   if (app.attributes['q-r-data']) {
-    rData = JSON.parse(app.attributes['q-r-data'].value);
+    try {
+      rData = JSON.parse(app.attributes['q-r-data'].value);
 
-    Object.keys(rData).forEach((key) => {
-      const keyArray = rData[key].split('.');
-      const initKeyValue = window[keyArray[0]];
-      const keyValue = initKeyValue && keyArray.length > 1 ?
-      // eslint-disable-next-line arrow-body-style
-        keyArray.reduce((obj, newKey, index) => {
-          return obj[newKey] || (index === keyArray.length - 1 ? undefined : obj);
-        }, initKeyValue)
-        : initKeyValue;
+      Object.keys(rData).forEach((key) => {
+        const keyArray = rData[key].split('.');
+        const initKeyValue = window[keyArray[0]];
+        const keyValue = initKeyValue && keyArray.length > 1 ?
+        // eslint-disable-next-line arrow-body-style
+          keyArray.reduce((obj, newKey, index) => {
+            return obj[newKey] || (index === keyArray.length - 1 ? undefined : obj);
+          }, initKeyValue)
+          : initKeyValue;
 
-      rData[key] = keyValue;
-    });
+        rData[key] = keyValue;
+      });
+    }
+    catch (e) {
+      // Do nothing
+    }
   }
 
   return _objectAssign({}, data, rData);
